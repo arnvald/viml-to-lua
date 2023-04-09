aadd2a127711c3aad92d43e1aac31dbf064b2295
Gregory Witek <hello@gwitek.com>
2023-04-09 16:44:25 +0200

    replace vim.o with vim.opt

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,62 +4,62 @@ vim.g.mapleader = ','
 vim.g.maplocalleader = '\\'
 
 -- basic settings
-vim.o.encoding = "utf-8"
-vim.o.backspace = "indent,eol,start" -- backspace works on every char in insert mode
-vim.o.completeopt = 'menuone,noselect'
-vim.o.history = 1000
-vim.o.dictionary = '/usr/share/dict/words'
-vim.o.startofline = true
+vim.opt.encoding = "utf-8"
+vim.opt.backspace = "indent,eol,start" -- backspace works on every char in insert mode
+vim.opt.completeopt = 'menuone,noselect'
+vim.opt.history = 1000
+vim.opt.dictionary = '/usr/share/dict/words'
+vim.opt.startofline = true
 
 -- Mapping waiting time
-vim.o.timeout = false
-vim.o.ttimeout = true
-vim.o.ttimeoutlen = 100
+vim.opt.timeout = false
+vim.opt.ttimeout = true
+vim.opt.ttimeoutlen = 100
 
 -- Display
-vim.o.showmatch  = true -- show matching brackets
-vim.o.scrolloff = 3 -- always show 3 rows from edge of the screen
-vim.o.synmaxcol = 300 -- stop syntax highlight after x lines for performance
-vim.o.laststatus = 2 -- always show status line
+vim.opt.showmatch  = true -- show matching brackets
+vim.opt.scrolloff = 3 -- always show 3 rows from edge of the screen
+vim.opt.synmaxcol = 300 -- stop syntax highlight after x lines for performance
+vim.opt.laststatus = 2 -- always show status line
 
-vim.o.list = false -- do not display white characters
-vim.o.foldenable = false
-vim.o.foldlevel = 4 -- limit folding to 4 levels
-vim.o.foldmethod = 'syntax' -- use language syntax to generate folds
-vim.o.wrap = false --do not wrap lines even if very long
-vim.o.eol = false -- show if there's no eol char
-vim.o.showbreak= '↪' -- character to show when line is broken
+vim.opt.list = false -- do not display white characters
+vim.opt.foldenable = false
+vim.opt.foldlevel = 4 -- limit folding to 4 levels
+vim.opt.foldmethod = 'syntax' -- use language syntax to generate folds
+vim.opt.wrap = false --do not wrap lines even if very long
+vim.opt.eol = false -- show if there's no eol char
+vim.opt.showbreak= '↪' -- character to show when line is broken
 
 -- Sidebar
-vim.o.number = true -- line number on the left
-vim.o.numberwidth = 3 -- always reserve 3 spaces for line number
-vim.o.signcolumn = 'yes' -- keep 1 column for coc.vim  check
-vim.o.modelines = 0
-vim.o.showcmd = true -- display command in bottom bar
+vim.opt.number = true -- line number on the left
+vim.opt.numberwidth = 3 -- always reserve 3 spaces for line number
+vim.opt.signcolumn = 'yes' -- keep 1 column for coc.vim  check
+vim.opt.modelines = 0
+vim.opt.showcmd = true -- display command in bottom bar
 
 -- Search
-vim.o.incsearch = true -- starts searching as soon as typing, without enter needed
-vim.o.ignorecase = true -- ignore letter case when searching
-vim.o.smartcase = true -- case insentive unless capitals used in search
+vim.opt.incsearch = true -- starts searching as soon as typing, without enter needed
+vim.opt.ignorecase = true -- ignore letter case when searching
+vim.opt.smartcase = true -- case insentive unless capitals used in search
 
-vim.o.matchtime = 2 -- delay before showing matching paren
-vim.o.mps = vim.o.mps .. ",<:>"
+vim.opt.matchtime = 2 -- delay before showing matching paren
+vim.opt.mps = vim.o.mps .. ",<:>"
 
 -- White characters
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.tabstop = 2 -- 1 tab = 2 spaces
-vim.o.shiftwidth = 2 -- indentation rule
-vim.o.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
-vim.o.expandtab = true -- expand tab to spaces
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2 -- 1 tab = 2 spaces
+vim.opt.shiftwidth = 2 -- indentation rule
+vim.opt.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
+vim.opt.expandtab = true -- expand tab to spaces
 
 -- Backup files
-vim.o.backup = true -- use backup files
-vim.o.writebackup = false
-vim.o.swapfile = false -- do not use swap file
-vim.o.undodir = HOME .. '/.vim/tmp/undo//'     -- undo files
-vim.o.backupdir = HOME .. '/.vim/tmp/backup//' -- backups
-vim.o.directory = '/.vim/tmp/swap//'   -- swap files
+vim.opt.backup = true -- use backup files
+vim.opt.writebackup = false
+vim.opt.swapfile = false -- do not use swap file
+vim.opt.undodir = HOME .. '/.vim/tmp/undo//'     -- undo files
+vim.opt.backupdir = HOME .. '/.vim/tmp/backup//' -- backups
+vim.opt.directory = '/.vim/tmp/swap//'   -- swap files
 
 vim.cmd([[
   au FileType python                  set ts=4 sw=4
@@ -67,11 +67,12 @@ vim.cmd([[
   au BufRead,BufNewFile *.ppmd        set ft=mkd tw=80 syntax=markdown
   au BufRead,BufNewFile *.markdown    set ft=mkd tw=80 syntax=markdown
   au BufRead,BufNewFile *.slimbars    set syntax=slim
+  au BufRead,BufNewFile *.vue         set syntax=vue
 ]])
 
 -- Commands mode
-vim.o.wildmenu = true -- on TAB, complete options for system command
-vim.o.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
+vim.opt.wildmenu = true -- on TAB, complete options for system command
+vim.opt.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
 
 -- Only show cursorline in the current window and in normal mode.
 vim.cmd([[
